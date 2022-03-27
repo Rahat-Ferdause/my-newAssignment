91c7c4816423659fadf256a6583a1f410d7f0f8e
Rahat-Ferdause <rahatferdause@gmail.com>
2022-03-27 23:51:08 +0600

    Final Commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Component/Header/Header";
 import Country from "./Component/Countries/Country";
+import Footer from "./Component/Footer/Footer";
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Country></Country>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1>Question:</h1>
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        <h1>Question1:How React Works?</h1>
+        <p>
+          Answer: It is maintained by Facebook. React uses a declarative
+          paradigm that makes it easier to reason about your application and
+          aims to be both efficient and flexible. It designs simple views for
+          each state in your application, and React will efficiently update and
+          render just the right component when your data changes
+        </p>
+      </div>
+      <div>
+        <h1>Question2:Props Vs State?</h1>
+        <p>
+          Answer: Props are used to pass data from one component to another. The
+          state is a local data storage that is local to the component only and
+          cannot be passed to other components.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
